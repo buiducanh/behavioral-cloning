92eb528897248a92c911679eb456b4e32c9adde4
Anh Bui <b.ducanh96@gmail.com>
2018-02-28 19:09:27 -0500

    fix batch size valid

--- model.py
+++ model.py
@@ -24,45 +24,45 @@ def network():
 
     # First Convolutional Layer
     model.add(Conv2D(nb_filter = 24, nb_row = 5, nb_col = 5, subsample = (2, 2)))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     # Second Convolutional Layer
     model.add(Conv2D(nb_filter = 36, nb_row = 5, nb_col = 5, subsample = (2, 2)))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     # Third Convolutional Layer
     model.add(Conv2D(nb_filter = 48, nb_row = 5, nb_col = 5, subsample = (2, 2)))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     # Fourth Convolutional Layer
     model.add(Conv2D(nb_filter = 64, nb_row = 3, nb_col = 3, subsample = (1, 1)))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     # Fifth Convolutional Layer
     model.add(Conv2D(nb_filter = 64, nb_row = 3, nb_col = 3, subsample = (1, 1)))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     model.add(Flatten())
 
     # First Fully Connected Layer
     model.add(Dense(100))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     # Second Fully Connected Layer
     model.add(Dense(50))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     # Third Fully Connected Layer
     model.add(Dense(10))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     # Output
     model.add(Dense(1))
